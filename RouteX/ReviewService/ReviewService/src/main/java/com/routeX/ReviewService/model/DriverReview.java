a1e566ddf51066ce0dd3ad76d06b192736aadf7c
meghraj75 <mnarkhede.02@gmail.com>
2025-02-06 14:11:46 +0530

    pk for each table in joined table inhertiance

--- RouteX/ReviewService/ReviewService/src/main/java/com/routeX/ReviewService/model/DriverReview.java
+++ RouteX/ReviewService/ReviewService/src/main/java/com/routeX/ReviewService/model/DriverReview.java
@@ -1,12 +1,15 @@
 package com.routeX.ReviewService.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Getter
 @Setter
 @Entity
+@PrimaryKeyJoinColumn(name = "driver_id")//pk of driver table
 public class DriverReview extends Review{
     private String driverReviewcmt;
 }
--- RouteX/ReviewService/ReviewService/src/main/java/com/routeX/ReviewService/model/PassengerReview.java
+++ RouteX/ReviewService/ReviewService/src/main/java/com/routeX/ReviewService/model/PassengerReview.java
@@ -1,12 +1,14 @@
 package com.routeX.ReviewService.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@PrimaryKeyJoinColumn(name = "passenger_id") // this is primary key of passenger table
 public class PassengerReview extends Review{
     private String passengerReviewContent;
 
